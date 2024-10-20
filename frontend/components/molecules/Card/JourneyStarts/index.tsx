ba9b1029564b5e2c3f25b54d12360b3f0dcf821b
Stephen-Kimoi <moenisteve@gmail.com>
2024-10-20 15:04:07 +0300

    modifying journey starts section

--- frontend/components/molecules/Card/JourneyStarts/index.tsx
+++ frontend/components/molecules/Card/JourneyStarts/index.tsx
@@ -12,21 +12,20 @@ interface TechJourneyStatsProps {
 }
 
 const TechJourneyStats: React.FC<TechJourneyStatsProps> = ({ title, stats, isDarkMode = false }) => {
-  // const bgColor = isDarkMode ? 'b' : '';
   const textColor = isDarkMode ? 'text-white' : 'text-black';
   const statValueColor = isDarkMode ? 'text-[#e2b134]' : 'text-[#571E1E]';
 
   return (
     <div className={`p-8 font-gordita max-w-2xl mx-auto ${textColor}`}>
-      <h2 className="text-4xl font-bold mb-6 leading-tight">
+      <h2 className="text-4xl font-bold mb-6 leading-tight tracking-[-0.08em]">
         <span className="font-gordita">{title}</span>
       </h2>
       
-      <div className="flex justify-between mt-8">
+      <div className="flex mt-8">
         {stats.map((stat, index) => (
-          <div key={index} className="text-center mr-6">
-            <p className={`font-bold text-[30px] ${statValueColor}`}>{stat.value}</p>
-            <p className="text-sm mt-2 uppercase">{stat.label}</p>
+          <div key={index} className="text-center m-2 items-center">
+            <p className={`font-bold text-[30px] ${statValueColor} tracking-[-0.11em]`}>{stat.value}</p>
+            <p className="text-sm mt-2 uppercase tracking-[-0.11em]">{stat.label}</p>
           </div>
         ))}
       </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -621,7 +621,7 @@ module.exports = {
       },
     },
     letterSpacing: {
-      tighter: '-0.05em',
+      tighter: '-0.11em',
       tight: '-0.025em',
       normal: '0em',
       wide: '0.025em',
