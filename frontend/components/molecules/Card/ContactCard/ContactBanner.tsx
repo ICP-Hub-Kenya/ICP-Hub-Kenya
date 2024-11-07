09efce2ae57727c54f48ea42d0a24f9b76b36f14
Roy Kiprop <roykiprop590@gmail.com>
2024-11-07 18:22:04 +0300

    Add telegram link

--- frontend/components/molecules/Card/ContactCard/ContactBanner.tsx
+++ frontend/components/molecules/Card/ContactCard/ContactBanner.tsx
@@ -2,22 +2,19 @@ import React from "react";
 import { FiInstagram, FiMail } from "react-icons/fi";
 import { BsTwitterX } from "react-icons/bs";
 import { TfiLinkedin } from "react-icons/tfi";
+import { BiLogoTelegram } from "react-icons/bi";
 import GradientButton from "../../../atoms/GradientButton/GradientButton";
 
 const ContactUs = () => {
   return (
     <div className="w-full max-w-6xl mx-auto my-10 px-6 md:px-12 py-8 md:py-16 rounded-2xl bg-gradient-to-r from-[#6a1d1b] to-[#340a0a] shadow-xl">
-      {/* Heading */}
       <h2 className="text-white text-3xl md:text-5xl font-bold mb-6 text-center tracking-tighter">
         Contact Us
       </h2>
 
-      {/* Content */}
-      <div className="mx-auto max-w-4xl flex flex-col md:flex-row items-center justify-between space-y-8 md:space-y-0">
-        {/* Left Section: Social Icons and Info */}
+      <div className="mx-auto max-w-4xl flex flex-col md:flex-row  justify-between space-y-8 md:space-y-0">
         <div className="flex flex-col items-start space-y-6 w-full md:w-2/5">
           <div className="flex items-center space-x-4">
-            {/* X Icon */}
             <div className="p-2 bg-white rounded-lg">
               <BsTwitterX size={25} />
             </div>
@@ -25,7 +22,6 @@ const ContactUs = () => {
           </div>
 
           <div className="flex items-center space-x-4">
-            {/* Instagram Icon */}
             <div className="p-2 bg-white rounded-lg">
               <FiInstagram size={25} />
             </div>
@@ -33,7 +29,6 @@ const ContactUs = () => {
           </div>
 
           <div className="flex items-center space-x-4">
-            {/* LinkedIn Icon */}
             <div className="p-2 bg-white rounded-lg">
               <TfiLinkedin size={25} />
             </div>
@@ -41,15 +36,19 @@ const ContactUs = () => {
           </div>
 
           <div className="flex items-center space-x-4">
-            {/* Email Icon */}
             <div className="p-2 bg-white rounded-lg">
               <FiMail size={25} />
             </div>
             <span className="text-white">info@icpkushite.com</span>
           </div>
+          <div className="flex items-center space-x-4">
+            <div className="p-2 bg-white rounded-lg">
+              <BiLogoTelegram size={25} />
+            </div>
+            <span className="text-white">ICP HUB Kenya</span>
+          </div>
         </div>
 
-        {/* Right Section: Contact Form */}
         <div className="w-full md:w-3/5 ">
           <form className="space-y-4">
             <input
