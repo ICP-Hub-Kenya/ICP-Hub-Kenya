e90da0e8eccf9661caba6bb92006d606cd92fa7f
Stephen-Kimoi <moenisteve@gmail.com>
2025-01-20 17:38:40 +0300

    finalizing our shop section

--- frontend/components/shop-components/product-card.tsx
+++ frontend/components/shop-components/product-card.tsx
@@ -9,7 +9,7 @@ interface ProductCardProps {
 export function ProductCard({ product }: ProductCardProps) {
   return (
     <Card className="overflow-hidden">
-      <CardContent className="p-4">
+      <CardContent className="p-0">
         <div className="aspect-square relative rounded-lg bg-muted group cursor-pointer">
           <Image src={product.imageUrl || "/placeholder.svg"} alt={product.title} fill className="object-cover" />
           <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center">
@@ -19,16 +19,16 @@ export function ProductCard({ product }: ProductCardProps) {
                 const message = encodeURIComponent(
                   `Hello, I would like to purchase this ${product.title} from ICP Hub Kenya Shop\n \nProduct Details:\n${product.title}\n ${window.location.origin}/${product.imageUrl}`
                 );
-                window.open(`https://wa.me/254791496764?text=${message}`, '_blank');
+                window.open(`https://wa.me/254707197333?text=${message}`, '_blank');
               }}              
             >
               Checkout
             </button>
           </div>
         </div>
-        <div className="p-4 bg-rose-50">
+        <div className="p-4 bg-rose-50 align-middle">
           <h3 className="font-medium">{product.title}</h3>
-          <p className="text-sm text-muted-foreground">{product.type}</p>
+          <p className="text-sm text-muted-foreground">{product.price} KES</p>
         </div>
       </CardContent>
     </Card>
